1e3520e86c22903b04ce7f84dd87b66fe70354b5
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-09 17:51:22 +0500

    added the start button functionality

--- app.js
+++ app.js
@@ -2,6 +2,45 @@ var min = 0;
 var sec = 0;
 var msec = 0;
 
+
 var minHeading = document.getElementById("min");
 var secHeading = document.getElementById("sec");
 var msecHeading = document.getElementById("msec");
+var interval;
+
+function timer() {
+    msec++
+    msecHeading.innerHTML = msec
+    if(msec >= 100)
+    {
+        sec++
+        secHeading.innerHTML = sec
+        msec = 0
+    }
+    else if(sec >= 60)
+    {
+        min++
+        minHeading.innerHTML = min
+        sec = 0;
+    }
+}
+
+function Start() {
+    interval = setInterval(timer, 10)
+    document.getElementById("startBtn").disabled = true;
+}
+
+function Pause() {
+    clearInterval(interval)
+    var btn = document.getElementById("startBtn").disabled = false;
+}
+
+function Reset() {
+    min = 0;
+    sec = 0;
+    msec = 0;
+    minHeading.innerHTML = min;
+    secHeading.innerHTML = sec;
+    msecHeading.innerHTML = msec;
+    Pause();
+}
