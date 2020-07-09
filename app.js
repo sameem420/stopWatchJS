6fda1d0952058ce2f40029fb8eaf9dbcc299af90
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-09 17:17:39 +0500

    added the style.css

---
+++ app.js
@@ -0,0 +1,7 @@
+var min = 0;
+var sec = 0;
+var msec = 0;
+
+var minHeading = document.getElementById("min");
+var secHeading = document.getElementById("sec");
+var msecHeading = document.getElementById("msec");
